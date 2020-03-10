5e5f6c999f316f7b3ef3ec3136e6b2383b857d65
Vladimirs Lahiza <vlahiza@rkh.co.uk>
2020-03-10 17:24:04 +0000

    snippets added

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,47 @@
 from django.shortcuts import render
 from django.db import models
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
 from wagtail.core.fields import StreamField
 from streams import blocks
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from django.shortcuts import render
+from wagtail.snippets.models import register_snippet
+
+class BlogAuthor(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+"
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("name"),
+                ImageChooserPanel("image")
+            ], heading="Name and Image",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("website"),
+            ], heading="Links"
+        )
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
+
+register_snippet(BlogAuthor)
 
 class BlogListingPage(RoutablePageMixin, Page):
     # Listing page lists all the Blog Detail Pages
@@ -19,6 +54,7 @@ class BlogListingPage(RoutablePageMixin, Page):
         context = super().get_context(request, *args, **kwargs)
         context["posts"] = BlogDetailPage.objects.live().public()
         context["special_link"] = self.reverse_subpage('latest_posts')
+        context["authors"] = BlogAuthor.objects.all()
         return context
 
     content_panels = Page.content_panels + [
